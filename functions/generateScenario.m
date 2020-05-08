56d65faf5a8815dd4c4f1e41a862dd0b601c6ef6
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-05-08 17:46:00 +0900

    added option to pass existing node position

--- functions/generateScenario.m
+++ functions/generateScenario.m
@@ -1,9 +1,8 @@
 %% 
 
-function [T,C,A,Atimes,tripsubset]=generateScenario(AcoordComplete,AtimesComplete,N,setlimits)
+function [T,C,A,Atimes,tripsubset]=generateScenario(AcoordComplete,AtimesComplete,N,setlimits,C)
 
-
-if nargin==4
+if nargin>=4 && ~isempty(setlimits)
     tripsubset=logical((AcoordComplete(:,1)>=setlimits(1,1)).*(AcoordComplete(:,1)<=setlimits(1,2)).* ...
                        (AcoordComplete(:,2)>=setlimits(2,1)).*(AcoordComplete(:,2)<=setlimits(2,2)).* ...
                        (AcoordComplete(:,3)>=setlimits(1,1)).*(AcoordComplete(:,3)<=setlimits(1,2)).* ...
@@ -18,64 +17,78 @@ if 0
     axis equal
 end
 
+%% create Atimes
 
-%% create C
-
-Acoords=AcoordComplete(tripsubset,:);
-[IDX,C,SUMD,D]=kmeans([Acoords(:,1:2);Acoords(:,3:4)],N);
-
-
-%% create A, Atimes
-
-A=reshape(IDX,sum(tripsubset),2);
 Atimes=AtimesComplete(tripsubset,:);
 
 
-%% create T
+if nargin<5
 
-TravelTimes=min(60,Atimes(:,2)-Atimes(:,1));
+    %% create C
 
-% fix travel times for trips ending next day
-TravelTimes(TravelTimes<-1380)=TravelTimes(TravelTimes<-1380)+1440;
-
-% remove errors/outliers
-ProblemTrips=(TravelTimes<0);
-TravelTimes(ProblemTrips)=[];
-A(ProblemTrips,:)=[];
-Atimes(ProblemTrips,:)=[];
-
-origSelector=zeros(length(A),N);
-destSelector=zeros(length(A),N);
-for i=1:N
-    origSelector(:,i)=logical(A(:,1)==i);
-    destSelector(:,i)=logical(A(:,2)==i);
-end
+    Acoords=AcoordComplete(tripsubset,:);
+    [IDX,C,SUMD,D]=kmeans([Acoords(:,1:2);Acoords(:,3:4)],N);
 
 
-T=zeros(N,N);
-for i=1:N
-    i
-    for j=1:N
-        Distances=TravelTimes(logical(origSelector(:,i).*destSelector(:,j)),:);
-        if ~isempty(Distances)
-            T(i,j)=mean(Distances);
+    %% create A
+
+    A=reshape(IDX,sum(tripsubset),2);
+
+
+    %% create T
+
+    TravelTimes=min(60,Atimes(:,2)-Atimes(:,1));
+
+    % fix travel times for trips ending next day
+    TravelTimes(TravelTimes<-1380)=TravelTimes(TravelTimes<-1380)+1440;
+
+    % remove errors/outliers
+    ProblemTrips=(TravelTimes<0);
+    TravelTimes(ProblemTrips)=[];
+    A(ProblemTrips,:)=[];
+    Atimes(ProblemTrips,:)=[];
+
+    origSelector=zeros(length(A),N);
+    destSelector=zeros(length(A),N);
+    for i=1:N
+        origSelector(:,i)=logical(A(:,1)==i);
+        destSelector(:,i)=logical(A(:,2)==i);
+    end
+
+
+    T=zeros(N,N);
+    for i=1:N
+        i
+        for j=1:N
+            Distances=TravelTimes(logical(origSelector(:,i).*destSelector(:,j)),:);
+            if ~isempty(Distances)
+                T(i,j)=mean(Distances);
+            end
         end
     end
-end
 
 
-%% reconstruct missing coordinates
+    %% reconstruct missing coordinates
 
-addpath('../CarSharingModel/utilities/dijkstra_alg');
-N=size(T,1);
-for i=1:N
-    for j=1:N
-        if T(i,j)==0
-            T(i,j)=dijkstra(T,i,j);
+    addpath('../CarSharingModel/utilities/dijkstra_alg');
+    N=size(T,1);
+    for i=1:N
+        for j=1:N
+            if T(i,j)==0
+                T(i,j)=dijkstra(T,i,j);
+            end
         end
     end
-end
 
+else 
+    
+    T=[];
+    
+    Acoord=AcoordComplete(tripsubset,:);
+    Distances=(Acoord(:,1)*ones(1,N)-ones(length(Acoord),1)*C(:,1)')^2+(Acoord(:,2)*ones(1,N)-ones(length(Acoord),1)*C(:,2)')^2;
+    [~,A]=min(Distances,[],2);
+    
+end
 
 return
 
