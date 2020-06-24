f784a7b39573e13cafbd1485fb179558679f05f9
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-06-24 20:17:31 +0900

    Update NYISO_fuelmix_data.m

--- functions/NYISO_fuelmix_data.m
+++ functions/NYISO_fuelmix_data.m
@@ -1,30 +1,31 @@
-% NYISO fuel mix data processing
+%% NYISO fuel mix data processing
+
+addpath functions utilities
 DataFolder=setDataFolder();
 
+% Year='2018';
+% MonthDays=[31 28 31 30 31 30 31 31 30 31 30 31];
 
-Year='2018';
-MonthDays=[31 28 31 30 31 30 31 31 30 31 30 31];
-
-% Year='2016';
-% MonthDays=[31 29 31 30 31 30 31 31 30 31 30 31];
+Year='2016';
+MonthDays=[31 29 31 30 31 30 31 31 30 31 30 31];
 
 MonthDaysC=cumsum([0 MonthDays]);
+
+%% load data
+
 f1=[];
 
 for m=1:12
     m
     for d=1:MonthDays(m)
-
         ThisDay=MonthDaysC(m)+d;
         Month=sprintf('%02d',m);
         Day=sprintf('%02d',d);
         FileName=[DataFolder 'input_files/NewYork/NYISO fuel mix/' Year Month Day 'rtfuelmix.csv'];
         g(ThisDay)=importdata(FileName);
         f1=[f1;g(ThisDay).data];
-
     end
 end
-
 
 Missing=0;
 f2=[];
@@ -37,11 +38,33 @@ for k=1:length(g)
 end
 
 f=f2;
+Fuels=g(1).textdata(2:8,3);
+GenerationByFuel_MW=reshape(f,length(Fuels),length(f)/length(Fuels))';
+
+%% adjust change of order on 25 january 
+% before 25 Jan:
+% Dual Fuel
+% Hydro
+% Natural Gas
+% Nuclear
+% Other Fossil Fuels
+% Other Renewables
+% Wind
+% ----
+% from 25 Jan: 
+% Dual Fuel
+% Natural Gas
+% Nuclear
+% Other Fossil Fuels
+% Other Renewables
+% Wind
+% Hydro
+ChangeMoment=24*12*24; % 24 January 24:00
+GenerationByFuel_MW_old=GenerationByFuel_MW;
+GenerationByFuel_MW=[GenerationByFuel_MW_old(1:ChangeMoment,:);GenerationByFuel_MW_old(ChangeMoment+1:end,[1 7 2 3 4 5 6])];
 
 % dual fuel mapped to natural gas, other renewables to biomass
 CarbonEmissionsByFuel=[490, 490, 12, 700, 230, 11, 24]';
-Fuels=g(1).textdata(2:8,3);
-GenerationByFuel_MW=reshape(f,length(Fuels),length(f)/length(Fuels))';
 CarbonEmissions_TonPerMin=GenerationByFuel_MW*CarbonEmissionsByFuel/1000/60; % ton/minute
 CarbonIntensity_kgPerMWh=CarbonEmissions_TonPerMin*1000*60./sum(GenerationByFuel_MW,2);
 
@@ -53,5 +76,25 @@ figure
 plot(CarbonIntensity_kgPerMWh)
 
 save([DataFolder 'grid/NYISO_fuelmix_' Year '.mat'],'Fuels','CarbonEmissionsByFuel','GenerationByFuel_MW','CarbonEmissions_TonPerMin','CarbonIntensity_kgPerMWh')
+
+
+%% generate file with prices and carbon intensity
+% results should be half-hourly
+
+addpath functions utilities
+DataFolder=setDataFolder();
+
+load([DataFolder 'grid/NYISO_fuelmix_2016.mat'],'CarbonIntensity_kgPerMWh');
+load('data/eleprices/DayaheadPrices2016NY.mat','NYDayAheadUSDMWh_2016');
+
+% prices
+x=repelem(reshape(NYDayAheadUSDMWh_2016(1:8760),24,365),2,1); % hourly
+
+% emissions
+y1=mean(reshape(CarbonIntensity_kgPerMWh(1:(8760*4)),2,8760*2)); % quarter hourly
+y=reshape(y1',48,365); 
+
+save('data/eleprices/NY_DA_2019.mat','x','y');
+
 
 
