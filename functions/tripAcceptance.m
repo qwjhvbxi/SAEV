70341e7b3e4bfc540a8a3cca800aed1de9fdbfed
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-06-30 17:04:14 +0900

    Update tripAcceptance.m

--- functions/tripAcceptance.m
+++ functions/tripAcceptance.m
@@ -1,16 +1,21 @@
 % A: OD matrix
 % prices: price matrix
 
-function newA=tripAcceptance(A,prices)
+function [newA,SelectedTrips]=tripAcceptance(A,prices)
 
 if sum(prices(:))==0
     
     newA=A;
+    SelectedTrips=[];
     return
     
 else
     
-    SelectedTrips=rand(size(A,1),1)>prices(sub2ind(size(prices),A(:,1),A(:,2)));
+    if numel(prices)>1
+        SelectedTrips=rand(size(A,1),1)>prices(sub2ind(size(prices),A(:,1),A(:,2)));
+    else
+        SelectedTrips=rand(size(A,1),1)>prices;
+    end
     newA=A(SelectedTrips,:);
 
 end
