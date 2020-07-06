fa7fa28e94c8717832d66514e22a38b55f25a273
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-07-06 16:31:01 +0900

    Update simul.m

--- run/EnergyPaper/simul.m
+++ run/EnergyPaper/simul.m
@@ -9,6 +9,20 @@
 % carbon price... ?
 
 
+%% prova
+Period=4:8;
+P=cpar('NYC2016');
+P.tripfolder='NYC2016'; % trips in 2019
+P.m=13000;
+P.gridfile='NY_DA_2016';
+gridoffset=0; % same year
+P.carbonprice=0;
+P.Operations.maxwait=Inf;
+% with optimized charging
+P.enlayeralg='aggregate';
+[S0a,R0a]=multiDaySim(Period,P,gridoffset);
+
+
 %% initializations
 
 % period
@@ -21,65 +35,67 @@ P.tripfolder='NYC2016'; % trips in 2019
 P.m=13000;
 
 
-
 %% new york grid
 
-P.gridfile='NY_DA_2016';
-gridoffset=0; % same year
+gridfile_a='NY_DA_2016';
+gridoffset_a=0; % same year
+P.gridfile=gridfile_a;
 P.carbonprice=0;
 
 % no optimization
 P.enlayeralg='no';
-[S1a,R1a]=multiDaySim(Period,P,gridoffset);
+[S1a,R1a]=multiDaySim(Period,P,gridoffset_a);
 
 % with optimized charging
 P.enlayeralg='aggregate';
-[S2a,R2a]=multiDaySim(Period,P,gridoffset);
+[S2a,R2a]=multiDaySim(Period,P,gridoffset_a);
 
 % with optimized charging and carbon pricing
 P.carbonprice=50; % [$/ton]
-[S3a,R3a]=multiDaySim(Period,P,gridoffset);
+[S3a,R3a]=multiDaySim(Period,P,gridoffset_a);
 
 
 
 %% germany grid
 
-P.gridfile='Germany_DA_2019';
-gridoffset=3; % 2016/01/01: Fri; 2019/01/01: Tue; 
+gridfile_b='Germany_DA_2019';
+gridoffset_b=3; % 2016/01/01: Fri; 2019/01/01: Tue; 
+P.gridfile=gridfile_b;
 P.carbonprice=0;
 
 % no optimization 
 P.enlayeralg='no';
-[S1b,R1b]=multiDaySim(Period,P,gridoffset);
+[S1b,R1b]=multiDaySim(Period,P,gridoffset_b);
 
 % with optimized charging
 P.enlayeralg='aggregate';
-[S2b,R2b]=multiDaySim(Period,P,gridoffset);
+[S2b,R2b]=multiDaySim(Period,P,gridoffset_b);
 
 % with optimized charging and carbon pricing
 P.carbonprice=50; % [$/ton]
-[S3b,R3b]=multiDaySim(Period,P,gridoffset);
+[S3b,R3b]=multiDaySim(Period,P,gridoffset_b);
 
 
 
 
 %% germany grid (summer)
 
-P.gridfile='Germany_DA_2019';
-gridoffset=3+7*20; % 2016/01/01: Fri; 2019/01/01: Tue; 
+gridfile_c='Germany_DA_2019';
+gridoffset_c=3+7*20; % 2016/01/01: Fri; 2019/01/01: Tue; 
+P.gridfile=gridfile_c;
 P.carbonprice=0;
 
 % no optimization 
 P.enlayeralg='no';
-[S1c,R1c]=multiDaySim(Period,P,gridoffset);
+[S1c,R1c]=multiDaySim(Period,P,gridoffset_c);
 
 % with optimized charging
 P.enlayeralg='aggregate';
-[S2c,R2c]=multiDaySim(Period,P,gridoffset);
+[S2c,R2c]=multiDaySim(Period,P,gridoffset_c);
 
 % with optimized charging and carbon pricing
 P.carbonprice=50; % [$/ton]
-[S3c,R3c]=multiDaySim(Period,P,gridoffset);
+[S3c,R3c]=multiDaySim(Period,P,gridoffset_c);
 
 
 
@@ -87,94 +103,113 @@ return
 
 %% plots
 
-figure('Units','centimeters','Position',[10,7,10,7])
-hold on
-plot(S1.cost+S1.emissions*50,'x-')
-plot(S2.cost+S2.emissions*50,'s-')
-plot(S3.cost,'o-')
-
-
-figure('Units','centimeters','Position',[10,7,10,7])
-hold on
-x=1:3;
-plot(x,[sum(S1.cost) , sum(S2.cost), sum(S3.cost-S3.emissions*50)]/length(Period),'s:','LineWidth',1.5)
-plot(x,[sum(S1.cost+S1.emissions*50) , sum(S2.cost+S2.emissions*50), sum(S3.cost)]/length(Period),'x:','LineWidth',1.5)
-yyaxis right
-plot(x,[sum(S1.emissions) , sum(S2.emissions), sum(S3.emissions)]/length(Period),'o:','LineWidth',1.5)
-xlim([0.5,3.5]);
-xticks(x);
-xticklabels({'B','O','O+CP'})
-legend({''})
-
-
-
-
-sum(S2.emissions)
-sum(S3.emissions)
+load(['data/eleprices/' gridfile_a '.mat'],'x','y');
+Ele_a=x;
+Emi_a=y;
+load(['data/eleprices/' gridfile_b '.mat'],'x','y');
+Ele_b=x;
+Emi_b=y;
+load(['data/eleprices/' gridfile_c '.mat'],'x','y');
+Ele_c=x;
+Emi_c=y;
 
 figure('Units','centimeters','Position',[10,7,10,7])
 hold on
-plot(S1.avgwait)
-plot(S2.avgwait)
-plot(S3.avgwait)
+plot(reshape(Ele_a(:,Period+gridoffset_a),length(Period)*48,1))
+plot(reshape(Ele_b(:,Period+gridoffset_b),length(Period)*48,1))
+plot(reshape(Ele_b(:,Period+gridoffset_c),length(Period)*48,1))
 
 figure('Units','centimeters','Position',[10,7,10,7])
 hold on
-plot(S1.dropped)
-plot(S2.dropped)
-plot(S3.dropped)
+plot(reshape(Emi_a(:,Period+gridoffset_a),length(Period)*48,1))
+plot(reshape(Emi_b(:,Period+gridoffset_b),length(Period)*48,1))
+plot(reshape(Emi_b(:,Period+gridoffset_c),length(Period)*48,1))
 
-figure
-hold on
-plot(S1.emissions)
-plot(S2.emissions)
-plot(S3.emissions)
+% figure('Units','centimeters','Position',[10,7,10,7])
+% hold on
+% plot(S1.cost+S1.emissions*50,'x-')
+% plot(S2.cost+S2.emissions*50,'s-')
+% plot(S3.cost,'o-')
+
+
+% figure('Units','centimeters','Position',[10,7,10,7])
+% hold on
+% x=1:3;
+% plot(x,[sum(S1.cost) , sum(S2.cost), sum(S3.cost-S3.emissions*50)]/length(Period),'s:','LineWidth',1.5)
+% plot(x,[sum(S1.cost+S1.emissions*50) , sum(S2.cost+S2.emissions*50), sum(S3.cost)]/length(Period),'x:','LineWidth',1.5)
+% yyaxis right
+% plot(x,[sum(S1.emissions) , sum(S2.emissions), sum(S3.emissions)]/length(Period),'o:','LineWidth',1.5)
+% xlim([0.5,3.5]);
+% xticks(x);
+% xticklabels({'B','O','O+CP'})
+% legend({''})
 
 
 
-figure
-hold on
-plot(sum(R2(2).Sim.e,2))
-plot(sum(R3(2).Sim.e,2))
-yyaxis right
-plot(R3(2).Params.co2)
+% 
+% sum(S2.emissions)
+% sum(S3.emissions)
+% 
+% figure('Units','centimeters','Position',[10,7,10,7])
+% hold on
+% plot(S1.avgwait)
+% plot(S2.avgwait)
+% plot(S3.avgwait)
+% 
+% figure('Units','centimeters','Position',[10,7,10,7])
+% hold on
+% plot(S1.dropped)
+% plot(S2.dropped)
+% plot(S3.dropped)
+% 
+% figure
+% hold on
+% plot(S1.emissions)
+% plot(S2.emissions)
+% plot(S3.emissions)
+% 
+% 
+% 
+% figure
+% hold on
+% plot(sum(R2(2).Sim.e,2))
+% plot(sum(R3(2).Sim.e,2))
+% yyaxis right
+% plot(R3(2).Params.co2)
 
 
 
 %% table
 
-% NY
+writeLine=@(S,Tax) fprintf('%d & %d & %.0f & %.3f & %.0f & %.2f \n\n',full(S.totwaitprctile(3)) , full(S.totwaitprctile(4)) , mean(S.cost+S.emissions*Tax) , sum(S.cost+S.emissions*Tax)/S.totminutes*60 , mean(S.emissions) , sum(S.emissions)/S.totminutes*10^6);
 
-% no carbon price
-[mean(S1a.cost) mean(S1a.emissions)]
-[mean(S2a.cost) mean(S2a.emissions)]
+writeLine(S0a,0)
 
-% $50 carbon tax
-[mean(S1a.cost+S1a.emissions*50) , mean(S1a.emissions)]
-[mean(S3a.cost) , mean(S3a.emissions)]
+% NY no carbon price
+writeLine(S1a,0)
+writeLine(S2a,0)
 
+% germany winter no carbon price
+writeLine(S1b,0)
+writeLine(S2b,0)
 
-% germany winter
-
-% no carbon price
-[mean(S1b.cost) mean(S1b.emissions)]
-[mean(S2b.cost) mean(S2b.emissions)]
-
-% $50 carbon tax
-[mean(S1b.cost+S1b.emissions*50) , mean(S1b.emissions)]
-[mean(S3b.cost) , mean(S3b.emissions)]
+% germany summer no carbon price
+writeLine(S1c,0)
+writeLine(S2c,0)
 
 
+% NY $50 carbon tax
+writeLine(S1a,50)
+writeLine(S3a,0)
 
-% germany summer
+% germany winter $50 carbon tax
+writeLine(S1b,50)
+writeLine(S3b,0)
 
-% no carbon price
-[mean(S1c.cost) mean(S1c.emissions)]
-[mean(S2c.cost) mean(S2c.emissions)]
+% germany summer $50 carbon tax
+writeLine(S1c,50)
+writeLine(S3c,0)
 
-% $50 carbon tax
-[mean(S1c.cost+S1c.emissions*50) , mean(S1c.emissions)]
-[mean(S3c.cost) , mean(S3c.emissions)]
 
 
 
