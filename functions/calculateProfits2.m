4f8d499b9bbedbdbb790cd54dfe5799fbe09838d
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-08-10 12:46:55 +0900

    integrate 2 functions into 1

--- functions/calculateProfits2.m
+++ functions/calculateProfits2.m
@@ -1,3 +1,6 @@
+%% [R,Rcost,U]=calculateProfits2(Pmat,Resmat,subsidy,penalty)
+% 
+
 function [R,Rcost,U]=calculateProfits2(Pmat,Resmat,subsidy,penalty)
 
 DroppedUtility=-15;
@@ -15,4 +18,33 @@ for d1=1:size(Resmat,1)
 %         U(d1,d2)=sum(Resmat(d1,d2).Economics.utility(logical(1-full(Resmat(d1,d2).Trips.dropped))))+sum(Resmat(d1,d2).Trips.dropped)*DroppedUtility;
         
     end
+end
+
+end
+
+
+
+%% [Revenue,Costs,Profits]=calculateCosts2(P,Res,tariff,travelcost)
+% travelcost and operatorcosts are in $/min, 
+
+function [Revenue,Costs,OperatingProfits]=calculateCosts3(P,Res,tariff,travelcost)
+
+[A,~,ASortInd,~,~]=generateGPStrips(P);
+
+A=reorderVectors(A,ASortInd);
+
+TraveledTrips=logical(full((Res.Sim.chosenmode==1).*(Res.Sim.dropped==0)));
+
+TraveledDistance=sum(Res.Params.Tr(sub2ind(size(Res.Params.Tr),A(TraveledTrips,1),A(TraveledTrips,2))));
+RelocationDistance=sum(Res.Sim.relodist);
+
+% calculate revenues
+Revenue=TraveledDistance*tariff;
+
+% calculate moving costs
+Costs=(TraveledDistance+RelocationDistance)*travelcost;
+
+
+OperatingProfits=Revenue-Costs;
+
 end
