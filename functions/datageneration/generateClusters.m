225919fda7ef87a6d548637ffb3c9c2d2dc0c201
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-08-24 16:19:08 +0900

    Update generateClusters.m

--- functions/datageneration/generateClusters.m
+++ functions/datageneration/generateClusters.m
@@ -5,26 +5,39 @@ function generateClusters(scenario,K,Plots)
 
 addpath functions
 DataFolder=setDataFolder();
-load([DataFolder 'scenarios/' scenario],'T','C')
 
-% find clusters
-[Clusters,CS]=kmeans(C,K);
+if nargin==2
 
-% find closest nodes to cluster centroids
-distances=(C(:,1)-CS(:,1)').^2+(C(:,2)-CS(:,2)').^2;
-[~,chargingStations]=min(distances);
-chargingStations=chargingStations';
+    load([DataFolder 'scenarios/' scenario],'T','C')
 
-save([DataFolder 'scenarios/' scenario '-' num2str(K) 'clusters'],'T','C','Clusters','chargingStations')
+    % find clusters
+    [Clusters,CS]=kmeans(C,K);
 
-if nargin>2 && Plots
-    figure
-    hold on
-    axis equal
-    scatter(C(:,1),C(:,2))
-    scatter(CS(:,1),CS(:,2))
-    scatter(C(chargingStations,1),C(chargingStations,2),'x')
-    scatter(C(Clusters==1,1),C(Clusters==1,2),'s')
+    % find closest nodes to cluster centroids
+    distances=(C(:,1)-CS(:,1)').^2+(C(:,2)-CS(:,2)').^2;
+    [~,chargingStations]=min(distances);
+    chargingStations=chargingStations';
+
+    save([DataFolder 'scenarios/' scenario '-' num2str(K) 'clusters'],'T','C','Clusters','chargingStations')
+
+else
+    
+    if Plots
+    
+        load([DataFolder 'scenarios/' scenario '-' num2str(K) 'clusters'],'T','C','Clusters','chargingStations')
+
+        c=2
+        
+        figure
+        hold on
+        scatter(C(:,1),C(:,2))
+%         scatter(CS(:,1),CS(:,2))
+        scatter(C(chargingStations,1),C(chargingStations,2),'filled')
+        scatter(C(Clusters==c,1),C(Clusters==c,2),'s')
+        axis equal tight
+    
+    end
+    
 end
 
 
