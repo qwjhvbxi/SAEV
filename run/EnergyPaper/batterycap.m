ecd31aad44cb96d28d20e3c56af081869ef7cce3
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-09-13 20:43:20 +0900

    update sensitivity runs

--- run/EnergyPaper/batterycap.m
+++ run/EnergyPaper/batterycap.m
@@ -11,7 +11,7 @@ gridoffsets=[0,-11,-11+7*20];    % 2016/01/01: Fri; 2019/01/01: Tue;
 P=cpar('NYC2016');
 P.Operations.maxwait=Inf; %20
 P.e=1;
-mvec=[5000 6000 8000 10000];
+bvec=[30 40 50 60];
 
 %% create Pmat
 
@@ -19,23 +19,23 @@ P.gridfile=gridfiles{1};
 P.carbonprice=0;
 
 P.enlayeralg='no';
-P.m=mvec(1);
+P.Tech.battery=bvec(1);
 Pmat{1}=P;
-P.m=mvec(2);
+P.Tech.battery=bvec(2);
 Pmat{2}=P;
-P.m=mvec(3);
+P.Tech.battery=bvec(3);
 Pmat{3}=P;
-P.m=mvec(4);
+P.Tech.battery=bvec(4);
 Pmat{4}=P;
 
 P.enlayeralg='aggregate';
-P.m=mvec(1);
+P.Tech.battery=bvec(1);
 Pmat{5}=P;
-P.m=mvec(2);
+P.Tech.battery=bvec(2);
 Pmat{6}=P;
-P.m=mvec(3);
+P.Tech.battery=bvec(3);
 Pmat{7}=P;
-P.m=mvec(4);
+P.Tech.battery=bvec(4);
 Pmat{8}=P;
 
 % P.gridfile=gridfiles{2};
@@ -77,15 +77,15 @@ Resolution=[];
 
 figure('Units','centimeters','Position',[10,7,10,7])
 hold on
-plot(mvec,[S(1:4).totavgwait],linevect{1})
-plot(mvec,[S(5:8).totavgwait],linevect{2})
+plot(bvec,[S(1:4).totavgwait],linevect{1})
+plot(bvec,[S(5:8).totavgwait],linevect{2})
 ylabel('mean waiting time (min)')
 % yyaxis right
 % plot(mvec,max([S(1:4).peakwait]),'bx--')
 % plot(mvec,max([S(5:8).peakwait]),'rs--')
 % ylabel('peak waiting time (min)')
 % xlim([0,24])
-xticks(mvec)
+xticks(bvec)
 xlabel('fleet size')
 legend({'on-demand','scheduled'},'Orientation','horizontal')
 set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
@@ -95,11 +95,11 @@ print([DataFolder 'figures/Energy/sens_fleet_wait'],Format,Resolution);
 figure('Units','centimeters','Position',[10,7,5,5])
 % figure('Units','centimeters','Position',[10,7,10,7])
 hold on
-plot(mvec,[S(1:4).totavgwait],linevect{1})
-plot(mvec,[S(5:8).totavgwait],linevect{2})
+plot(bvec,[S(1:4).totavgwait],linevect{1})
+plot(bvec,[S(5:8).totavgwait],linevect{2})
 ylabel('mean waiting time (min)')
-xticks(mvec)
-xticklabels([int2str(mvec'/1000) ones(length(mvec),1)*'k'])
+xticks(bvec)
+xticklabels([int2str(bvec'/1000) ones(length(bvec),1)*'k'])
 xlabel('fleet size')
 grid on
 legend({'on-demand','scheduled'},'Orientation','vertical')
@@ -108,13 +108,13 @@ print([DataFolder 'figures/Energy/sens_fleet_wait-small'],Format,Resolution);
 
 figure('Units','centimeters','Position',[10,7,10,7])
 hold on
-plot(mvec,mean([S(1:4).cost]),linevect{1})
+plot(bvec,mean([S(1:4).cost]),linevect{1})
 % plot(([S(1:4).cost]'),'*')
-plot(mvec,mean([S(5:8).cost]),linevect{2})
+plot(bvec,mean([S(5:8).cost]),linevect{2})
 % plot(([S(5:8).cost]'),'*')
 ylabel('cost per day ($)')
 % xlim([0,24])
-xticks(mvec)
+xticks(bvec)
 xlabel('fleet size')
 legend({'on-demand','scheduled'},'Orientation','horizontal','Location','East')
 set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
@@ -122,13 +122,13 @@ print([DataFolder 'figures/Energy/sens_fleet_cost'],Format,Resolution);
 
 figure('Units','centimeters','Position',[10,7,5,5])
 hold on
-plot(mvec,mean([S(1:4).cost])/1000,linevect{1})
-plot(mvec,mean([S(5:8).cost])/1000,linevect{2})
+plot(bvec,mean([S(1:4).cost])/1000,linevect{1})
+plot(bvec,mean([S(5:8).cost])/1000,linevect{2})
 ylabel('cost per day (thousand $)')
 % xlim([0,24])
 grid on
-xticks(mvec)
-xticklabels([int2str(mvec'/1000) ones(length(mvec),1)*'k'])
+xticks(bvec)
+xticklabels([int2str(bvec'/1000) ones(length(bvec),1)*'k'])
 xlabel('fleet size')
 legend({'on-demand','scheduled'},'Orientation','vertical','Location','East')
 set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
@@ -136,16 +136,16 @@ print([DataFolder 'figures/Energy/sens_fleet_cost-small'],Format,Resolution);
 
 figure('Units','centimeters','Position',[10,7,10,7])
 hold on
-plot(mvec,mean([S(1:4).cost]),linevect{1})
-plot(mvec,mean([S(5:8).cost]),linevect{2})
+plot(bvec,mean([S(1:4).cost]),linevect{1})
+plot(bvec,mean([S(5:8).cost]),linevect{2})
 ylabel('cost per day ($)')
 yyaxis right
-plot(mvec,[S(1:4).totavgwait],linevect{1})
-plot(mvec,[S(5:8).totavgwait],linevect{2})
+plot(bvec,[S(1:4).totavgwait],linevect{1})
+plot(bvec,[S(5:8).totavgwait],linevect{2})
 ylabel('mean waiting time (min)')
 % ylabel('peak waiting time (min)')
 % xlim([0,24])
-xticks(mvec)
+xticks(bvec)
 xlabel('fleet size')
 legend({'on-demand','scheduled'},'Orientation','horizontal')
 set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
--- run/EnergyPaper/sensitivity.m
+++ run/EnergyPaper/sensitivity.m
@@ -2,11 +2,11 @@
 
 addpath plots functions utilities
 
-[P,R]=generateplotline3('NYC2016',[],'Operations.maxwait',[Inf],'m',13000,'TransportLayer.ts',10:16,'TransportLayer.tr',6:18);
-
-
-
-[~,Rw]=generateplotline3('NYC2016','peakwait','Operations.maxwait',[Inf],'m',13000,'TransportLayer.ts',10:16,'TransportLayer.tr',6:18);
-
+% [P,R]=generateplotline3('NYC2016',[],'Operations.maxwait',[Inf],'m',13000,'TransportLayer.ts',10:16,'TransportLayer.tr',6:18);
+% [~,Rw]=generateplotline3('NYC2016','peakwait','Operations.maxwait',[Inf],'m',13000,'TransportLayer.ts',10:16,'TransportLayer.tr',6:18);
 % best values: ts = 12-13; tr = 7-10 (24 to 26 minutes; 14 to 20 minutes)
 % chosen values: ts=12, tr=10
+
+
+
+
