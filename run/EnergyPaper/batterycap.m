6b073158201c1b6bed8148dcacd017481c9ebd5f
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-09-16 15:05:16 +0900

    new files for sensitivity

--- run/EnergyPaper/batterycap.m
+++ run/EnergyPaper/batterycap.m
@@ -11,6 +11,8 @@ gridoffsets=[0,-11,-11+7*20];    % 2016/01/01: Fri; 2019/01/01: Tue;
 P=cpar('NYC2016');
 P.Operations.maxwait=Inf; %20
 P.e=1;
+P.m=5000;
+% P.m=10000;
 bvec=[30 40 50 60];
 
 %% create Pmat
@@ -69,11 +71,43 @@ for k=1:length(Pmat)
 end
 
 
-%% plots
+%% main plots
 
 linevect={'x-','s-','v-','^-','d-','>--','<--','o--','x--'};
 Format='-depsc2';
 Resolution=[];
+
+figure('Units','centimeters','Position',[10,7,5,5])
+% figure('Units','centimeters','Position',[10,7,10,7])
+hold on
+plot(bvec,[S(1:4).totavgwait],linevect{1})
+plot(bvec,[S(5:8).totavgwait],linevect{2})
+ylabel('mean waiting time (min)')
+xticks(bvec)
+% xticklabels([int2str(bvec') ones(length(bvec),1)*'k'])
+xlabel('fleet size')
+grid on
+legend({'on-demand','scheduled'},'Orientation','vertical')
+set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
+print([DataFolder 'figures/Energy/sens_battery_wait-small'],Format,Resolution);
+
+figure('Units','centimeters','Position',[10,7,5,5])
+hold on
+plot(bvec,mean([S(1:4).cost])/1000,linevect{1})
+plot(bvec,mean([S(5:8).cost])/1000,linevect{2})
+ylabel('cost per day (thousand $)')
+% xlim([0,24])
+grid on
+xticks(bvec)
+% xticklabels([int2str(bvec'/1000) ones(length(bvec),1)*'k'])
+xlabel('fleet size')
+legend({'on-demand','scheduled'},'Orientation','vertical','Location','East')
+set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
+print([DataFolder 'figures/Energy/sens_battery_cost-small'],Format,Resolution);
+
+return
+
+%% other plots
 
 figure('Units','centimeters','Position',[10,7,10,7])
 hold on
@@ -89,22 +123,7 @@ xticks(bvec)
 xlabel('fleet size')
 legend({'on-demand','scheduled'},'Orientation','horizontal')
 set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
-print([DataFolder 'figures/Energy/sens_fleet_wait'],Format,Resolution);
-
-
-figure('Units','centimeters','Position',[10,7,5,5])
-% figure('Units','centimeters','Position',[10,7,10,7])
-hold on
-plot(bvec,[S(1:4).totavgwait],linevect{1})
-plot(bvec,[S(5:8).totavgwait],linevect{2})
-ylabel('mean waiting time (min)')
-xticks(bvec)
-xticklabels([int2str(bvec'/1000) ones(length(bvec),1)*'k'])
-xlabel('fleet size')
-grid on
-legend({'on-demand','scheduled'},'Orientation','vertical')
-set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
-print([DataFolder 'figures/Energy/sens_fleet_wait-small'],Format,Resolution);
+print([DataFolder 'figures/Energy/sens_battery_wait'],Format,Resolution);
 
 figure('Units','centimeters','Position',[10,7,10,7])
 hold on
@@ -118,21 +137,7 @@ xticks(bvec)
 xlabel('fleet size')
 legend({'on-demand','scheduled'},'Orientation','horizontal','Location','East')
 set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
-print([DataFolder 'figures/Energy/sens_fleet_cost'],Format,Resolution);
-
-figure('Units','centimeters','Position',[10,7,5,5])
-hold on
-plot(bvec,mean([S(1:4).cost])/1000,linevect{1})
-plot(bvec,mean([S(5:8).cost])/1000,linevect{2})
-ylabel('cost per day (thousand $)')
-% xlim([0,24])
-grid on
-xticks(bvec)
-xticklabels([int2str(bvec'/1000) ones(length(bvec),1)*'k'])
-xlabel('fleet size')
-legend({'on-demand','scheduled'},'Orientation','vertical','Location','East')
-set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
-print([DataFolder 'figures/Energy/sens_fleet_cost-small'],Format,Resolution);
+print([DataFolder 'figures/Energy/sens_battery_cost'],Format,Resolution);
 
 figure('Units','centimeters','Position',[10,7,10,7])
 hold on
@@ -149,7 +154,7 @@ xticks(bvec)
 xlabel('fleet size')
 legend({'on-demand','scheduled'},'Orientation','horizontal')
 set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
-print([DataFolder 'figures/Energy/sens_fleet_combined'],Format,Resolution);
+print([DataFolder 'figures/Energy/sens_battery_combined'],Format,Resolution);
 
 % k=2;
 % % [S0,R]=multiDaySim(18,Pmat{k},GridOffsetsVec(k),1);
