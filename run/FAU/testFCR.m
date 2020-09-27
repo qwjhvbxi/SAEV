afeffeed0e2523905cfb57139dd4f482044ca39e
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-09-27 20:25:39 +0900

    divided inputs

--- run/FAU/testFCR.m
+++ run/FAU/testFCR.m
@@ -1,13 +1,18 @@
 function [FailMinutes,DeltaPower]=testFCR(P,R)
-DataFolder=setDataFolder();
-load([DataFolder 'grid/' P.FCR.filename],'f');
-ReshapeFactor=size(f,1)/1440*P.e;
-f=average2(f(:,P.gridday),ReshapeFactor);
-Limits=P.FCR.limits;
 
-FCRe=min(1,max(-1,(1-(f-Limits(1))/(Limits(2)-Limits(1))*2))); % needed FCR
-FCRe=FCRe(1:1440);
-PowerExpect=FCRe*P.FCR.contracted*1000;
+FileName=P.FCR.filename;
+GridDay=P.gridday;
+Limits=P.FCR.limits;
+Contracted=P.FCR.contracted;
 Power=sum(R.Sim.ef,2);
+
+DataFolder=setDataFolder();
+load([DataFolder 'grid/' FileName],'f');
+ReshapeFactor=size(f,1)/(length(Power));
+F=average2(f(:,GridDay),ReshapeFactor);
+
+FCRe=min(1,max(-1,(1-(F-Limits(1))/(Limits(2)-Limits(1))*2))); % needed FCR
+FCRe=FCRe(1:1440);
+PowerExpect=FCRe*Contracted*1000;
 DeltaPower=sparse(double(round(Power-PowerExpect,7)));
 FailMinutes=(sum(abs(DeltaPower)>0));
