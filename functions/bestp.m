79aa40df7f57be4d470318c2ab4a69e7f66c2f37
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-09-27 19:29:54 +0900

    Update bestp.m

--- functions/bestp.m
+++ functions/bestp.m
@@ -1,9 +1,9 @@
 
-function p=bestp(c)
+function p=bestp(dista,gamma_r,gamma_alt)
 
 P=0:0.001:0.5;
-R=@(p,c) (  exp(-p.*c)./(exp(-p.*c)+exp(-0.25*c))  ).*(p-0.1).*c; % net revenues at certain price and distance
-[~,pind]=max(R(P,c));
+R=@(p,d) (  exp(-d*p)./(exp(-d*p)+exp(-gamma_alt*d))  ).*(d*(p-gamma_r)); % net revenues at certain price and distance
+[~,pind]=max(R(P,dista),[],2);
 p=P(pind);
 
 end
