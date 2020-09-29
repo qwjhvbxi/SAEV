921f6df901a7f5e78f64501b401eb4313f96f02e
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-09-30 01:22:36 +0900

    updated test

--- run/FAU/testFCR.m
+++ run/FAU/testFCR.m
@@ -11,8 +11,15 @@ load([DataFolder 'grid/' FileName],'f');
 ReshapeFactor=size(f,1)/(length(Power));
 F=average2(f(:,GridDay),ReshapeFactor);
 
-FCRe=min(1,max(-1,(1-(F-Limits(1))/(Limits(2)-Limits(1))*2))); % needed FCR
+FCRe=min(1,max(-1,(F-50)/(Limits(2)-Limits(1))*2)); % needed FCR (EV POV)
 FCRe=FCRe(1:1440);
 PowerExpect=FCRe*Contracted*1000;
-DeltaPower=sparse(double(round(Power-PowerExpect,7)));
+DeltaPower=sparse(double(round(Power-PowerExpect)));
 FailMinutes=(sum(abs(DeltaPower)>0));
+
+if 1
+figure
+hold on 
+plot(PowerExpect)
+plot(Power)
+end
