5b0a7ef0d5b45f9c678e2199f2c21558cd8af28f
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-02-09 00:07:07 +0900

    updated folder location

--- plots/plotresults.m
+++ plots/plotresults.m
@@ -153,10 +153,10 @@ lon3=lon3/pi*180;
 
 [lat3 ;lon3]
 
-
+DataFolder=setDataFolder();
 % M=imread('data/TokyoStreetMapNarrow.png');
 % M=imread('data/TokyoStreetMapSmall2.png');
-M=imread('data/TokyoStreetMap5km.png');
+M=imread([DataFolder 'input_files\Tokyo\maps/TokyoStreetMap5km.png']);
 % image([0 10],[0 10],flipud(M)/1.5)
 % image([0 10],[0 10],254/2+flipud(M)/2)
 image([0 5],[0 5],254/2+flipud(M)/2)
