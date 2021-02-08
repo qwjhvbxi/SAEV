8653dcfa6bfce01487e7ac625c3727158c301ed6
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-02-09 00:06:53 +0900

    Update plotmap.m

--- plots/plotmap.m
+++ plots/plotmap.m
@@ -1,3 +1,9 @@
-s=scatter(A(:,1),A(:,2),'filled'); 
+
+function plotmap(A,c)
+s=scatter(A(:,1),A(:,2),1,'filled'); 
+s.MarkerFaceColor=c;
 s.MarkerFaceAlpha=0.05;
 axis equal tight
+box on
+% set(gca,'FontUnits','points','FontWeight','normal','FontName','Times')
+end
