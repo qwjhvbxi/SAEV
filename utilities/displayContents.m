f32623a3162d523cc75b9c0e0f64f035d3e6d086
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-04-09 10:40:11 +0900

    exported function

---
+++ utilities/displayContents.m
@@ -0,0 +1,22 @@
+function Files=displayContents(folderaddress)
+
+DirFiles=dir(folderaddress);
+q=0;
+
+fprintf('Files in folder: \n\n')
+
+for i=1:length(DirFiles)
+    if ~strcmp(DirFiles(i).name,'.') && ~strcmp(DirFiles(i).name,'..') && DirFiles(i).isdir==0
+        q=q+1;
+        Files(q)=DirFiles(i);
+        fprintf('%d. %s \n',q,DirFiles(i).name(1:end-4));
+    end
+end
+
+if q==0
+    fprintf('Empty folder. \n\n')
+    Files=[];
+else
+    fprintf('\n')
+end
+end
