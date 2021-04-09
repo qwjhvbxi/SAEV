87af7a368d7de786000d3acbe4b9dcb57fcbb093
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-04-09 10:43:11 +0900

    parallel implementation

--- functions/populateT.m
+++ functions/populateT.m
@@ -1,15 +1,18 @@
 %% T=populateT(T)
 % reconstruct missing coordinates (==0) with Dijkstra algorithm
 
-function T=populateT(T)
+function newT=populateT(T)
 
 addpath('utilities/dijkstra_alg');
 N=size(T,1);
-for i=1:N
+newT=T;
+parfor i=1:N
     i
     for j=1:N
-        if T(i,j)==0
-            T(i,j)=dijkstra(T,i,j);
+        if i~=j
+            if T(i,j)==0
+                newT(i,j)=dijkstra(T,i,j);
+            end
         end
     end
 end
