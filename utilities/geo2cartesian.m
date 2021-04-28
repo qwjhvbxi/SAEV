0da33f550107f7d5cadd07240825fe7640f10322
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-04-29 01:40:08 +0900

    better name

--- utilities/geo2cartesian.m
+++ utilities/geo2cartesian.m
@@ -1,8 +1,9 @@
-%% convert geodesic coordinates to approximate cartesian coordinates (km)
+%% ACartesian=GEO2CARTESIAN(Ageodesic,BoundariesGeodesic,ApproxLatitude)
+% Convert geodesic coordinates to approximate cartesian coordinates (km)
 % Ageodesic=[Lon,Lat] (Nx2)
 % BoundariesGeodesic=[min Lat, min Lon] (1x2)
 % ApproxLatitude=Lat (1x1) 
 
-function ACartesian=convertToCartesian(Ageodesic,BoundariesGeodesic,ApproxLatitude)
+function ACartesian=geo2cartesian(Ageodesic,BoundariesGeodesic,ApproxLatitude)
     ACartesian=[(deg2km(Ageodesic(:,1)-min(BoundariesGeodesic(:,1))))*cos(deg2rad(ApproxLatitude)), deg2km(Ageodesic(:,2)-min(BoundariesGeodesic(:,2)))];
 end
