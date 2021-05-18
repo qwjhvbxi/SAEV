d27496206e253be05a221caeccd420d482c6ffa4
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-05-18 17:03:45 +0900

    add distance matrix

--- functions/getscenario.m
+++ functions/getscenario.m
@@ -3,13 +3,13 @@
 % 
 % See also: mainsim
 
-function [T,clusters,chargingStations]=getscenario(scenario)
+function [T,clusters,chargingStations,D]=getscenario(scenario)
 
 DataFolder=getdatafolder();
 
 %% load distance matrix
 
-load([DataFolder 'scenarios/' scenario '.mat'],'T','Clusters','chargingStations');
+load([DataFolder 'scenarios/' scenario '.mat'],'T','C','D','Clusters','chargingStations');
 
 
 %% setup clustering
@@ -21,7 +21,7 @@ end
 if ~exist('T','var')
     k=strfind(scenario,'_');
     scenarioRoot=scenario(1:k(end)-1);
-    load([DataFolder 'scenarios/' scenarioRoot '.mat'],'T');
+    load([DataFolder 'scenarios/' scenarioRoot '.mat'],'T','C','D');
 end
 
 % number of nodes 
@@ -35,4 +35,19 @@ if ~exist('Clusters','var')
     chargingStations=(1:n)';
     clusters=(1:n)';
 end
+
+
+%% distance
+
+if ~exist('D','var') 
+    if exist('C','var')
+        D=[]; % TODO: calculate approximate distance from coordinates
+    else
+        D=[];
+    end
+end
+
+
+
+
 
