4d904a2b90c40ff9de27b7fbb7cf064685ba364a
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-05-31 10:34:00 +0900

    Create generatedistancemat.m

---
+++ utilities/generatedistancemat.m
@@ -0,0 +1,13 @@
+%% Generate distance matrix from coordinates
+% D=generatedistancemat(C)
+% 
+
+function D=generatedistancemat(C)
+
+D=sqrt((C(:,1)-C(:,1)').^2+(C(:,2)-C(:,2)').^2);
+
+end
+
+% % test
+% a=[0,0;1,1;-1,1];
+% generatedistancemat(a)
