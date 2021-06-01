20e21024e456fa00322d61574a65cd5721cc29cb
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-06-01 12:06:57 +0900

    simplified function to work with aggregates

--- functions/computeemd.m
+++ functions/computeemd.m
@@ -1,16 +1,27 @@
-%% [dkemd,dkod]=COMPUTEEMD(A,Atimes,T,Beta,relocationNodes)
+%% [dkemd,dkod]=COMPUTEEMD(fo,fd,T,Beta,relocationNodes)
 % Compute travel time for relocation during time horizon.
+% 'fo' and 'fd' are respectively the number of departures and arrivals at
+% each node at each time interval. Size: [minutes x no. nodes]
+% 'T' is the distance matrix.
 % 'Beta' is the length (in minutes) of one interval in the horizon.
 % 'relocationNodes' is the index of the nodes which are relocation
 % destinations (associated with A).
 % 
 % See also: mainsim, computetraveltime
 
-function [dkemd,dkod]=computeemd(A,Atimes,T,Beta,relocationNodes)
+function [dkemd,dkod]=computeemd(fo,fd,T,Beta,relocationNodes)
 
 fprintf('\n Calculating approximate relocation distance\n\n')
 
-t=double(max(Atimes(:)));
+[thisT]=gettraveltimenow(T,0);
+if nargin<5
+    n=size(thisT,1);
+    relocationNodes=(1:n)';
+end
+nr=length(relocationNodes);
+approx=(nr>100);
+
+t=size(fo,1);
 mtsim=round(t/Beta);
 
 dkemd=zeros(mtsim,1);
@@ -23,21 +34,11 @@ for kt=1:mtsim
     fprintf('\n %d/%d\n\n',kt,mtsim)
     
     [thisT]=gettraveltimenow(T,kt*Beta);
-    
-    if nargin<5
-        n=size(thisT,1);
-        relocationNodes=(1:n)';
-    end
     thisTrelocation=thisT(relocationNodes,relocationNodes);
-    nr=size(thisTrelocation,1);
-    approx=(nr>100);
-    
-    % find trips in this time step
-    thisStep=logical((Atimes(:,1)>=(kt-1)*Beta+1).*(Atimes(:,1)<=kt*Beta));
-    
-    % feeder and receiver stations
-    F=accumarray([A(thisStep,2);nr],[ones(sum(thisStep),1);0])';
-    R=accumarray([A(thisStep,1);nr],[ones(sum(thisStep),1);0])';
+
+    thisStep=(kt-1)*Beta+1:kt*Beta;
+    F=sum(fd(thisStep,:));
+    R=sum(fo(thisStep,:));
 
     % identify optimal relocation flux
     x=optimalrelocationfluxes(F,R,thisTrelocation,60,approx);
