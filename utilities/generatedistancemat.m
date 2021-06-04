266999f7b8b503a53074a35617ba95eef3e7eb46
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-06-04 14:32:25 +0900

    added option for two matrices

--- utilities/generatedistancemat.m
+++ utilities/generatedistancemat.m
@@ -1,9 +1,15 @@
 %% Generate distance matrix from coordinates
 % D=generatedistancemat(C) 
+% D=generatedistancemat(C1,C2)
+% C is a n x 2 matrix with Euclidean coordinates
 
-function D=generatedistancemat(C)
+function D=generatedistancemat(C1,C2)
 
-D=sqrt((C(:,1)-C(:,1)').^2+(C(:,2)-C(:,2)').^2);
+if nargin<2
+    C2=C1;
+end
+
+D=sqrt((C1(:,1)-C2(:,1)').^2+(C1(:,2)-C2(:,2)').^2);
 
 end
 
