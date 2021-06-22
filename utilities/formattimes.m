a8c828439d8b26d08bfb78283869541401ac8641
Riccardo Iacobucci <iacobucci.riccardo@gmail.com>
2021-06-22 10:50:35 +0900

    added hour formatting

--- utilities/formattimes.m
+++ utilities/formattimes.m
@@ -1,6 +1,10 @@
 %% a=FORMATTIMES(seconds)
-% Return string with formatted times in the form m:ss
+% Return string with formatted times in the form m:ss or h:mm:ss
 
 function a=formattimes(seconds)
-    a=sprintf('%d:%0.2d',floor(seconds/60),round(rem(seconds,60)));
+    if seconds<3600
+        a=sprintf('%d:%0.2d',floor(seconds/60),round(rem(seconds,60)));
+    else
+        a=sprintf('%dh%0.2d:%0.2d',floor(seconds/3600),round(rem(seconds/60,60)),round(rem(seconds,60)));
+    end
 end
