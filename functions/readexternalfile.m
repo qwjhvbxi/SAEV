26d4706205d4571aec612bb1b2b0e82c8ab28191
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-06-23 16:36:24 +0900

    fixed bugs

--- functions/readexternalfile.m
+++ functions/readexternalfile.m
@@ -4,6 +4,10 @@
 function [Column1,Column2,Resolution]=readexternalfile(FileName,Day,MinuteSample)
 
 fileID=fopen(FileName,'r');
+if fileID==-1
+    warning('File not found.');
+    return;
+end
 H=textscan(fileID,'%q %f',1,'Delimiter',','); % retrieve resolution info
 T=textscan(fileID,'%f %f','Delimiter',',','HeaderLines',2); % retrieve values
 fclose(fileID);
@@ -24,13 +28,15 @@ Interval=PointsPerDay*(Day-1)+1:PointsPerDay*(Day+1);
 IntervalAdjusted=rem(Interval-1,TotalDays*PointsPerDay)+1;
 
 if MinuteSample
-% repeat values to find per minute values
-Column1=repelem(T{1}(IntervalAdjusted),Resolution,1);
-Column2=repelem(T{2}(IntervalAdjusted),Resolution,1);
-Resolution=1;
+    % repeat values to find per minute values
+    Column1=repelem(T{1}(IntervalAdjusted),Resolution,1);
+    Column2=repelem(T{2}(IntervalAdjusted),Resolution,1);
+    Resolution=1;
+else
+    Column1=T{1}(IntervalAdjusted);
+    Column2=T{2}(IntervalAdjusted);
 end
 
 % replace nan values with zeros
+Column1(isnan(Column1))=0;
 Column2(isnan(Column2))=0;
-
-
