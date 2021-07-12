e4bf0b9ebd43d3ce22c26d9d8f5972853f29a4c2
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-07-12 15:59:21 +0900

    return bounding box; bug fix

--- utilities/geo2cartesian.m
+++ utilities/geo2cartesian.m
@@ -4,12 +4,12 @@
 % BoundariesGeodesic=[min Lat, min Lon] (1x2)
 % ApproxLatitude=Lat (1x1) 
 
-function ACartesian=geo2cartesian(Ageodesic,BoundariesGeodesic,ApproxLatitude)
+function [ACartesian,BoundariesGeodesic]=geo2cartesian(Ageodesic,BoundariesGeodesic,ApproxLatitude)
     if nargin<3
         ApproxLatitude=mean(Ageodesic(:,2));
     end
     if nargin<2
-        BoundariesGeodesic=min(Ageodesic);
+        BoundariesGeodesic=min(Ageodesic,[],1);
     end
     ACartesian=[(deg2km(Ageodesic(:,1)-min(BoundariesGeodesic(:,1))))*cos(deg2rad(ApproxLatitude)), deg2km(Ageodesic(:,2)-min(BoundariesGeodesic(:,2)))];
 end
