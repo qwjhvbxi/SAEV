9b246525ee105828505ee73d6045de51b0001d47
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-08-17 17:27:08 +0900

    added more options

--- functions/generatetripsmodalshift.m
+++ functions/generatetripsmodalshift.m
@@ -2,34 +2,39 @@
 % With dependence on trip distance
 % OriginalModes={'bicycle','car','foot','public_transport'}
 % OriginalModes={'bicycle','car','foot','public_transport','car passenger'}
+% ModeShift
 
 function generatetripsmodalshift(oldTripName,newTripName,tripDay,ModeShift,D)
 
 addpath functions utilities
 DataFolder=getdatafolder();
 
-newFolder=[DataFolder 'trips/' newTripName];
-if ~exist(newFolder,'dir')
-    mkdir(newFolder)
-end
-
 originalFile=[DataFolder 'trips/' oldTripName '/d' num2str(tripDay)];
 load(originalFile,'A','Atimes');
 
+if nargin==5
+    res=0.5; % resolution
+    tripDistancesKm=D(sub2ind(size(D),A(:,1),A(:,2)))/1000;
+    distIndex=min(size(ModeShift,2),max(1,floor(tripDistancesKm/res)));
+    IND=sub2ind(size(ModeShift),A(:,3),distIndex);
+    
+    % select trips depending on mode and distance
+    ToSelect=rand(length(IND),1)<ModeShift(IND);
+else
+    % no distance dependence
+    ModeShift=ModeShift(:,1);
+    ToSelect=rand(length(A),1)<ModeShift(A(:,3));
+end
 
-
-res=0.5; % resolution
-tripDistancesKm=D(sub2ind(size(D),A(:,1),A(:,2)))/1000;
-distIndex=min(size(ModeShift,2),max(1,floor(tripDistancesKm/res)));
-
-IND=sub2ind(size(ModeShift),A(:,3),distIndex);
-
-% select trips depending on mode and distance
-ToSelect=rand(length(IND),1)<ModeShift(IND);
 A=A(ToSelect,:);
 Atimes=Atimes(ToSelect,:);
 
 [A,Atimes,~]=cleantripdata(A,Atimes,1);
+
+newFolder=[DataFolder 'trips/' newTripName];
+if ~exist(newFolder,'dir')
+    mkdir(newFolder)
+end
 
 save([newFolder '/d' num2str(tripDay)],'A','Atimes');
 
