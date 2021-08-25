fae3ca7178d731c20b60bfe906a0d1443ab919d1
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-08-25 12:33:06 +0900

    half size pics

--- utilities/prettyfigure.m
+++ utilities/prettyfigure.m
@@ -1,6 +1,10 @@
-function prettyfigure
+function prettyfigure(Half)
 
-figure('Units','centimeters','Position',[10,7,10,7])
+if nargin>0 && Half
+    figure('Units','centimeters','Position',[10,7,7,5])
+else
+    figure('Units','centimeters','Position',[10,7,10,7])
+end
 set(gca,'FontUnits','points','FontWeight','normal','FontSize',11,'FontName','Times');
 box on
 
