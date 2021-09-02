63c6d382c0ca47267a2ef4db9dcb171ff5ce3a23
Riccardo Iacobucci <iacobucci.riccardo@gmail.com>
2021-09-02 11:08:16 +0900

    Update generatescenarioclusters.m

--- functions/generatescenarioclusters.m
+++ functions/generatescenarioclusters.m
@@ -1,6 +1,8 @@
-%% generateClusters(scenario,K,J[,kmed])
-% generate a new scenario file with J charging stations and K clusters found with
-% k-medoids (kmed=1) or k-means. The new scenario is named 'scenario_J-K'
+%% generateClusters(scenario,K[,J,kmed])
+% generate a new scenario file with K clusters and J charging stations
+% found with k-medoids (kmed=1, default) or k-means (kmed=0). The new
+% scenario is named 'scenario_K-J'. If only K is provided, charging
+% stations are placed in the cluster centers.
 
 function generatescenarioclusters(scenario,K,J,kmed)
 
@@ -27,23 +29,30 @@ tempC(nanT,:)=[];
 if kmed
 
     % generate clusters
-    [IDX,~,~,~,nodeIdClusters] =kmedoids(tempC,K);
+    [IDX,~,~,~,nodeIdClusters]=kmedoids(tempC,K);
     
-    % generate charging stations
-    [~,~,~,~,nodeIdCs] =kmedoids(tempC,J);
+    if K==J
+        nodeIdCs=nodeIdClusters;
+    else
+        % generate charging stations
+        [~,~,~,~,nodeIdCs] =kmedoids(tempC,J);
+        %     [~,CS2,~,D,nodeIdCs0] =kmedoids(CS,J);
+        %     nodeIdCs=nodeIdClusters(nodeIdCs0)    
+    end
     
-%     [~,CS2,~,D,nodeIdCs0] =kmedoids(CS,J);
-%     nodeIdCs=nodeIdClusters(nodeIdCs0)
-    
-else
+else % k-means
     
     % generate clusters
     [IDX,~,~,D] =kmeans(tempC,K);
     [~,nodeIdClusters]=min(D); % find closest node to station
     
-    % generate charging stations
-    [~,~,~,D] =kmeans(tempC,J);
-    [~,nodeIdCs]=min(D); % find closest node to station
+    if K==J
+        nodeIdCs=nodeIdClusters;
+    else
+        % generate charging stations
+        [~,~,~,D] =kmeans(tempC,J);
+        [~,nodeIdCs]=min(D); % find closest node to station
+    end
     
 end
 
@@ -55,5 +64,5 @@ for i=1:length(IDX)
     Clusters(realT(i))=IDX(i);
 end
 
-save([DataFolder 'scenarios/' scenario '_' num2str(K)],'Clusters','clusterIDs','chargingStations')
+save([DataFolder 'scenarios/' scenario '_' num2str(K) '-' num2str(J)],'Clusters','clusterIDs','chargingStations')
 
